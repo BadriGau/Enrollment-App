98086a54b1ad9be965ca66b6741715ed18a77205
Badri <badrigautam19@gmail.com>
2021-08-15 02:55:00 +0200

    nav set up

--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,18 @@
 from application import app
-from flask import render_template
+from flask import render_template,url_for
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    return render_template("index.html",login=False)
+
+@app.route("/courses")
+def courses():
+    return "<p>courses</p>"
+
+@app.route("/register")
+def register():
+    return "<p>courses</p>"
+
+@app.route("/login")
+def login():
+    return "<p>courses</p>"
