c780d2c7116238f86a8160a7e22b819681c7b8a3
Badri <badrigautam19@gmail.com>
2021-08-17 14:45:48 +0200

    show json data

--- application/routes.py
+++ application/routes.py
@@ -3,16 +3,18 @@ from flask import render_template,url_for
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html",login=False)
+    return render_template("index.html",index=True)
 
 @app.route("/courses")
 def courses():
-    return render_template("courses.html",login=False)
+    courseData = [{"courseID":"1","title":"PHP","description":"Web development with php","credits":"4","term":"Spring"},
+                  {"courseID":"2","title":"Python","description":"Web development with Python","credits":"5","term":"Summer"}]
+    return render_template("courses.html",courseData=courseData,courses=True)
 
 @app.route("/register")
 def register():
-    return render_template("register.html",login=False)
+    return render_template("register.html",register=True)
 
 @app.route("/login")
 def login():
-    return render_template("login.html",login=False)
+    return render_template("login.html",login=True)
